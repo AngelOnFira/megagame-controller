ec8c5eb975107d3830deed55f7c7924a496e1c2d
Forest Anderson <forestkzanderson@gmail.com>
2021-07-15 08:47:07 -0400

    Models for teams

--- django/player/models.py
+++ django/player/models.py
@@ -6,3 +6,7 @@ from django.db.models.signals import post_save
 class Player(models.Model):
     wallet = models.OneToOneField(Wallet, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, default="")
+
+    team = models.ForeignKey(
+        "team.Team", on_delete=models.CASCADE, null=True, blank=True
+    )
--- django/team/models.py
+++ django/team/models.py
@@ -1,12 +1,8 @@
 from django.db import models
-from bot.users import models as user_models
 
 # Create your models here.
 class Team(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    players = models.ManyToManyField('users.Member', related_name='teams')
-    
