23102da8eddcf735c96ac7bc46cf7bd70adef520
Forest Anderson <forestkzanderson@gmail.com>
2021-08-03 21:52:19 -0400

    Transaction frontend

--- frontend/src/components/Transactions.vue
+++ frontend/src/components/Transactions.vue
@@ -13,13 +13,27 @@
         <q-item v-for="transaction in transactions" :key="transaction.id">
           <q-item-section>
             <q-item-label
+              v-if="
+                transaction.from_wallet === null &&
+                transaction.to_wallet === null
+              "
+              >?? -> ??</q-item-label
+            >
+            <q-item-label v-else-if="transaction.to_wallet === null"
+              >{{ transaction.from_wallet.name }} -> ??</q-item-label
+            >
+            <q-item-label v-else
               >{{ transaction.from_wallet.name }} ->
               {{ transaction.to_wallet.name }}</q-item-label
             >
-            <q-item-label caption lines="2"
+            <q-item-label
+              caption
+              lines="2"
+              v-if="transaction.currency !== null"
               >{{ transaction.amount }}
               {{ transaction.currency.name }}</q-item-label
             >
+            <q-item-label caption lines="2" v-else>X ??</q-item-label>
           </q-item-section>
           <q-item-section side top>
             <q-item-label caption>{{ transaction.elapsed }}</q-item-label>
